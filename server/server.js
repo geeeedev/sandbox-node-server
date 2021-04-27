4a1f7a28cf25831e2b8633bed4b9918c8f0666f2
Gwennie Lau <geeeedev@gmail.com>
2021-04-26 21:36:16 -0700

    config server

--- server/server.js
+++ server/server.js
@@ -2,20 +2,20 @@ const port = 7000;
 const db_name = "dbJokes";
 // const cors = require('cors');    //not needed right now as I have no front end
 const express = require('express');
+
+
+//set up express and enabling req.body parsing
 const app = express();
-
-
-
-//enabling request.body parsing
 app.use(express.json());
-//load routes file
-//...
+// app.use(cors());
 
-//load mongoose connection file
-
-
+//load routes file with app
+require('./routes/...')(app);
 
 //ready for server to listen
 app.listen(port, () =>{
     console.log(`Web Server established - listening for requests on port ${port} to respond to ...`);
 })
+
+//load mongoose connection file
+require('./config/...')(db_name);
