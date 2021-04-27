5a3376fcfd8115200ee5c26126a345ba311891db
Gwennie Lau <geeeedev@gmail.com>
2021-04-26 22:02:24 -0700

    config mongoose

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = (db_name) => {
-    mongoose.connect(`mongodb://localhost/${db_name}`,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindeAndModify: false,
+  mongoose
+    .connect(`mongodb://localhost/${db_name}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindeAndModify: false,
     })
-    .then((res)=>{
-        console.log(`Connected to ${db_name}`);
-        console.log(`Conn Success Response: `, res);
+    .then((res) => {
+      console.log(`Connected to ${db_name}`);
+      console.log(`Conn Success Response: `, res);
     })
-    .catch((err)=>{
-        console.log(`Connection to ${db_name} Failed`, err);
+    .catch((err) => {
+      console.log(`Connection to ${db_name} Failed`, err);
     });
 };
