b27e6d931fcf3e81d8fc8a10406b1a419d4405e2
Gwennie Lau <geeeedev@gmail.com>
2021-04-28 21:25:08 -0700

    backend practice

---
+++ serverPrdMgr/server.js
@@ -0,0 +1,12 @@
+const port = 6000;
+const dbName = "dbPrdMgr";
+const express = require("express");
+const cors = require("cors");
+
+//db connection setup
+
+//server setup
+
+//route setup
+
+//server listens
